aff01f97215e36990ae4199b1148c4276d73f4f2
KnightsWhoSayNi <johngodman006@gmail.com>
2026-03-04 23:37:59 -0500

    not pretty but the db conn works

--- db/init.sql
+++ db/init.sql
@@ -1,18 +1,30 @@
 create schema lober;
 
+create table lober.users (
+    id          serial primary key,
+    username    varchar(32),
+    email       varchar(32),
+    team_id     serial
+);
+create table lober.teams (
+    id          serial primary key,
+    name        varchar(32),
+    color       varchar(6),
+    lead_id     serial
+);
 create table lober.c2s (
-                     id      serial primary key,
-                     name    varchar(32)
+    id          serial primary key,
+    name        varchar(32)
 );
 create table lober.scope (
-                      id      serial primary key,
-                      name    varchar(32)
+    id          serial primary key,
+    name        varchar(32)
 );
 create table lober.events (
-                     id          serial primary key,
-                     name        varchar(32),
-                     command_run varchar(256),
-                     c2_id       serial,
-                     scope_id    serial,
-                     time        timestamp default current_timestamp
+    id          serial primary key,
+    command     varchar(256),
+    user_id     serial,
+    c2_id       serial,
+    scope_id    serial,
+    time        timestamp default current_timestamp
 );
--- main.go
+++ main.go
@@ -2,43 +2,71 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 )
 
 type Event struct {
-	Name    string    `json:"name"`
+	ID      int64
 	Command string    `json:"command"`
+	User    string    `json:"user"`
 	C2      string    `json:"c2"`
 	Scope   string    `json:"scope"`
 	Time    time.Time `json:"time"`
 }
 
-func main() {
-	// impl env lookup
-	dsn := "postgres://lober:lober@localhost:5432/lober?sslmode=disable"
-	db, err := sql.Open("postgres", dsn)
+var db *sql.DB
+var err error
 
+func init() {
+	// impl env lookup
+	dsn := "postgres://lober:lober@db:5432/lober?sslmode=disable"
+	db, err = sql.Open("postgres", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Connected to postgres")
+}
 
+func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		rows, _ := db.Query("select * from events")
-		c.JSON(200, gin.H{
-			"message": rows,
-		})
-	})
-
+	router.GET("/events", getEvents)
 	router.POST("/events", newEvent)
 
 	router.Run()
+}
+
+func getEvents(c *gin.Context) {
+	rows, err := db.Query("select * from events")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	defer rows.Close()
+
+	events := make([]Event, 0)
+
+	for rows.Next() {
+		var e Event
+		if err := rows.Scan(&e.ID, &e.Command, &e.User, &e.C2, &e.Scope, &e.Time); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		events = append(events, e)
+	}
+
+	if err = rows.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, events)
 }
 
 func newEvent(c *gin.Context) {
@@ -50,5 +78,15 @@ func newEvent(c *gin.Context) {
 	}
 
 	event.Time = time.Now()
+
+	tx, _ := db.Begin()
+	stmt, _ := tx.Prepare(pq.CopyIn("events", "command", "user_id", "c2_id", "scope_id", "time"))
+	stmt.Exec(event.Command, 1, 2, 3, event.Time)
+	stmt.Exec()
+	stmt.Close()
+	tx.Commit()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
 	c.JSON(http.StatusOK, event)
 }
