a8c0484008ca4180faa627d5bbe3b42606c0aeb9
KnightsWhoSayNi <johngodman006@gmail.com>
2026-03-05 14:00:17 -0500

    PoC if i've ever seen one

--- main.go
+++ main.go
@@ -54,10 +54,15 @@ func getEvents(c *gin.Context) {
 
 	for rows.Next() {
 		var e Event
-		if err := rows.Scan(&e.ID, &e.Command, &e.User, &e.C2, &e.Scope, &e.Time); err != nil {
+		x, y, z := 0, 0, 0
+		if err := rows.Scan(&e.ID, &e.Command, &x, &y, &z, &e.Time); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
+		getUsername(x, &e.User)
+		getC2Name(y, &e.C2)
+		getScopeName(z, &e.Scope)
+		fmt.Printf("%s ran by %s using %s on %s at %s\n", e.Command, e.User, e.C2, e.Scope, e.Time)
 		events = append(events, e)
 	}
 
@@ -77,11 +82,15 @@ func newEvent(c *gin.Context) {
 		return
 	}
 
+	userId, c2Id, scopeId := 0, 0, 0
+	getUserId(event.User, &userId)
+	getC2Id(event.C2, &c2Id)
+	getScopeId(event.Scope, &scopeId)
 	event.Time = time.Now()
 
 	tx, _ := db.Begin()
 	stmt, _ := tx.Prepare(pq.CopyIn("events", "command", "user_id", "c2_id", "scope_id", "time"))
-	stmt.Exec(event.Command, 1, 2, 3, event.Time)
+	stmt.Exec(event.Command, userId, c2Id, scopeId, event.Time)
 	stmt.Exec()
 	stmt.Close()
 	tx.Commit()
@@ -89,4 +98,24 @@ func newEvent(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
 	c.JSON(http.StatusOK, event)
+}
+
+func getUserId(username string, ptr *int) {
+	db.QueryRow("select id from users where username = $1", username).Scan(ptr)
+}
+func getC2Id(c2 string, ptr *int) {
+	db.QueryRow("select id from c2s where name = $1", c2).Scan(ptr)
+}
+func getScopeId(scope string, ptr *int) {
+	db.QueryRow("select id from scope where name = $1", scope).Scan(ptr)
+}
+
+func getUsername(id int, ptr *string) {
+	db.QueryRow("select username from users where id = $1", id).Scan(ptr)
+}
+func getC2Name(id int, ptr *string) {
+	db.QueryRow("select name from c2s where id = $1", id).Scan(ptr)
+}
+func getScopeName(id int, ptr *string) {
+	db.QueryRow("select name from scope where id = $1", id).Scan(ptr)
 }
