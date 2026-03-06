4ceff86897d08e861bf20cd6219bca7da91fc9de
KnightsWhoSayNi <johngodman006@gmail.com>
2026-03-05 21:25:49 -0500

    fix mod; refactor later

--- main.go
+++ main.go
@@ -7,10 +7,42 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/KnightsWhoSayNi/lober/db"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
 )
+
+type Event struct {
+	ID      int64
+	Command string    `json:"command"`
+	User    string    `json:"user"`
+	C2      string    `json:"c2"`
+	Scope   string    `json:"scope"`
+	Time    time.Time `json:"time"`
+}
+
+type User struct {
+	ID       int64
+	Username string
+	Email    string
+	Team     string // todo; users can be on multiple teams
+}
+
+type Team struct {
+	ID    int64
+	Name  string
+	Color string
+	Lead  string
+}
+
+type C2 struct {
+	ID   int64
+	Name string
+}
+
+type Scope struct {
+	ID   int64
+	Name string
+}
 
 var db *sql.DB
 var err error
